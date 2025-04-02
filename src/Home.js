2261e1d84e682dcf6773f2de534fb4aebabd370b
mn01001920890 <prof.mahmoudmn@gmail.com>
2025-04-02 05:22:09 +0300

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+
+// باقي الكود سيتم نسخه من التعديل النهائي في الكانفاس لاحقًا (مؤقتًا فراغ)
+
+export default function Home() {
+  return <div>الرئيسية</div>;
+}
