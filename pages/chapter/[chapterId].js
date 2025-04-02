b326a227826238c4e4bc0748741bd8a189834cd2
mn01001920890 <prof.mahmoudmn@gmail.com>
2025-04-02 20:55:17 +0300

    Delete pages directory

--- pages/chapter/[chapterId].js
+++
@@ -1,8 +0,0 @@
-import { useRouter } from 'next/router';
-import { ChapterPage } from '../../src/Home';
-
-export default function ChapterRoute() {
-  const router = useRouter();
-  if (!router.isReady || !router.query.chapterId) return null;
-  return <ChapterPage chapterId={router.query.chapterId} />;
-}
